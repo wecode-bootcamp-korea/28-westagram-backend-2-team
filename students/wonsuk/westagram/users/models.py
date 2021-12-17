0d3e588010342867cdf24f3d6d46662fe55630b7
지원석 <jiwonsuk@jiwonseog-ui-MacBook-Air.local>
2021-12-17 12:46:36 +0900

    edited users' models

--- students/wonsuk/westagram/users/models.py
+++ students/wonsuk/westagram/users/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
 class User(models.Model):
-    name = models.CharField(max_length=50)
-    email = models.CharField(max_length=300)
-    password = models.CharField(max_length=100)
-    phone_number = models.CharField(max_length=100)
-    other_information = models.CharField(max_length=300)
+    username          = models.CharField(max_length=50)
+    email             = models.CharField(max_length=100, unique=True)
+    password          = models.CharField(max_length=256)
+    phone_number      = models.CharField(max_length=100)
+    other_information = models.CharField(max_length=300, null=True)
     
     class Meta:
         db_table = 'users'

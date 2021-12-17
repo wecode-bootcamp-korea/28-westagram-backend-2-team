e5c1b6c55f5c99e4d16f8158f88a30709d1514e0
kang min sung <kangminsung@gangminseong-ui-MacBookPro.local>
2021-12-17 10:56:29 +0900

    Mission 2 수정

--- students/minsung/users/models.py
+++ students/minsung/users/models.py
@@ -3,11 +3,11 @@ from django.db import models
 # Create your models here.
 
 class User(models.Model):
-    name         = models.CharField(max_length=100)
-    email        = models.CharField(max_length=200)
-    password     = models.IntegerField()
-    phone_number = models.IntegerField()
-    age          = models.IntegerField()
+    name          = models.CharField(max_length=50)
+    email         = models.CharField(max_length=100, unique=True)
+    password      = models.CharField(max_length=30)
+    phone_number  = models.CharField(max_length=30, unique=True)
+    date_of_birth = models.DateField(null=True)
 
     class Meta:
         db_table = 'users'

227d2a24246dad94a11fc0f00829a35a6240626a
지원석 <jiwonsuk@jiwonseog-ui-MacBook-Air.local>
2021-12-17 17:19:57 +0900

    added age info in user Class and two codes about DateTime through the Kyunghun's comments

--- students/wonsuk/westagram/users/models.py
+++ students/wonsuk/westagram/users/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 
 class User(models.Model):
-    username          = models.CharField(max_length=50)
-    email             = models.CharField(max_length=100, unique=True)
-    password          = models.CharField(max_length=256)
-    phone_number      = models.CharField(max_length=100)
-    other_information = models.CharField(max_length=300, null=True)
+    username     = models.CharField(max_length=50)
+    email        = models.CharField(max_length=100, unique=True)
+    password     = models.CharField(max_length=256)
+    phone_number = models.CharField(max_length=100)
+    age          = models.IntegerField(null=True)
+    
+    created_at   = models.DateTimeField(auto_now_add=True)
+    updated_at   = models.DateTimeField(auto_now=True)
     
     class Meta:
         db_table = 'users'

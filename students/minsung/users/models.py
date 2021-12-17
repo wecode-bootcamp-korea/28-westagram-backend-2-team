ab06d151664860b98c5bf441ae0f8ab3ef7e0b32
kang min sung <kangminsung@gangminseong-ui-MacBookPro.local>
2021-12-17 11:47:50 +0900

    Mission 2: 수정2

--- students/minsung/users/models.py
+++ students/minsung/users/models.py
@@ -1,11 +1,9 @@
 from django.db import models
 
-# Create your models here.
-
 class User(models.Model):
     name          = models.CharField(max_length=50)
     email         = models.CharField(max_length=100, unique=True)
-    password      = models.CharField(max_length=30)
+    password      = models.CharField(max_length=256)
     phone_number  = models.CharField(max_length=30, unique=True)
     date_of_birth = models.DateField(null=True)
 

bb1f1ca838c0b85f754b082cb46394036cb1d5ce
chanyoung <cyl0504@gmail.com>
2021-12-18 14:59:21 +0900

    Modify : models.py

--- students/chan/westagram/users/models.py
+++ students/chan/westagram/users/models.py
@@ -4,13 +4,13 @@ from django.db.models import Model
 # Create your models here.
 class User(models.Model):
     name                     = models.CharField(max_length=45)
-    email                    = models.EmailField(max_length=128, null=False, unique=True)
-    password                 = models.CharField(max_length=200, null=False)
-    phone_number              = models.CharField(max_length=11, null=False)
+    email                    = models.EmailField(max_length=128, unique=True)
+    password                 = models.CharField(max_length=200)
+    phone_number             = models.CharField(max_length=11)
     business_number          = models.CharField(max_length=50, blank=True)
     is_allowing_email_recive = models.BooleanField(default=True)
     is_enterprize            = models.BooleanField(default=True)
-    personal_site            = models.URLField(max_length=200, blank=True)
+    personal_site            = models.URLField(max_length=1000, blank=True)
     created_at               = models.DateField(auto_now_add=True)
     updated_at               = models.DateTimeField(auto_now=True)
 

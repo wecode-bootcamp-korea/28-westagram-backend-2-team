b6c081c0b9134ac37756957a80fdd5e7d5692659
지원석 <jiwonsuk@jiwonseog-ui-MacBook-Air.local>
2021-12-20 17:09:03 +0900

    created SignUp view and added 10 users' information

---
+++ students/wonsuk/westagram/users/validators.py
@@ -0,0 +1,13 @@
+import re
+
+from django.core.exceptions import ValidationError
+
+def email_regex_match(email):
+    EMAIL_REGEX = "^[a-zA-Z0-9+-\_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
+    if not re.match(EMAIL_REGEX, email):
+        raise ValidationError('INVALID_EMAIL')
+    
+def password_regex_match(password):
+    PASSWORD_REGEX = "^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$"
+    if not re.match(PASSWORD_REGEX, password):
+        raise ValidationError('INVALID_PASSWORD')
---
+++ students/wonsuk/westagram/users/views.py
@@ -0,0 +1,34 @@
+import json
+
+from django.core.exceptions import ValidationError
+from django.http            import JsonResponse
+from django.views           import View
+from users.models           import User
+from users.validators       import email_regex_match, password_regex_match
+
+class SignUpView(View):
+    def post(self, request):
+        try:
+            data = json.loads(request.body)
+            
+            email_regex_match(data['email'])
+            password_regex_match(data['password'])
+            
+            if User.objects.filter(email=data['email']).exists():
+                return JsonResponse({'message' : 'EMAIL_ALREADY_EXISTS'}, status=401)
+
+            User.objects.create(
+                                username     = data['username'],
+                                email        = data['email'],
+                                password     = data['password'],
+                                phone_number = data['phone_number'],
+                                age          = data['age']
+                                )
+            return JsonResponse({'message' : 'SUCCESS'}, status=201)
+
+        except KeyError:
+            return JsonResponse({'message' : 'KEY_ERROR'}, status=400)
+        
+        except ValidationError as e:
+            return JsonResponse({'message' : e.message}, status=401)
+        

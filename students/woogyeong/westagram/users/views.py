6b6afccd4807d441fb0102e645d0b1db941b4ae6
Bruno-Jang <sosdnrud@gmail.com>
2021-12-20 13:53:45 +0900

    Modify - SignUpView : validation(simplify)

--- students/woogyeong/westagram/users/views.py
+++ students/woogyeong/westagram/users/views.py
@@ -1,6 +1,5 @@
 # python built-in module
 import json
-import re
 
 # 외부 module
 from django.http  import JsonResponse
@@ -8,41 +7,21 @@ from django.views import View
 
 # 사용자 module (직접 작성한 module)
 from users.models import User
+from users.validators import validate_email, validate_password
 
 class SignUpView(View):
     def post(self, request):
         data = json.loads(request.body)
         
-        def validate_email(email=data['email']):
-            email_regex      = re.compile(r"^[a-zA-Z]+[!#$%&'*+-/=?^_`(){|}~]*[a-zA-Z0-9]*@[\w]+\.[a-zA-Z0-9-]+[.]*[a-zA-Z0-9]+$")
-            email_validation = email_regex.match(email)
-            
-            if email_validation:
-                return True
-            else:
-                return False
-        
-        def validate_password(password=data['password']):
-            password_regex = re.compile(r'([\w!@#$%^&*(),.?\":{}|<>]+){8}')
-            password_valid = password_regex.match(password)
-            
-            if password_valid:
-                return True
-            else:
-                return False
-        
         try:
-            if User.objects.filter(email=data['email']).exists():
-                return JsonResponse({'message': 'USER_EXISTS'}, status=409)
-            else:
-                if validate_email() == True and validate_password() == True:
-                    pass
-                elif validate_email() == False:
-                    return JsonResponse({'message': 'INVALID_EMAIL'}, status=409)
-                elif validate_password() == False:
-                    return JsonResponse({'message': 'INVALID_PASSWORD'}, status=409)
-                
-                user = User.objects.create(
+            if not validate_email(data['email']):
+                return JsonResponse({'message': 'Email format is not valid'}, status=400)
+            elif not validate_password(data['password']):
+                return JsonResponse({'message': 'Password format is not valid'}, status=400)
+            elif User.objects.filter(email=data['email']).exists():
+                return JsonResponse({'message': 'USER_EXISTS'}, status=400)
+            elif validate_email(data['email']) and validate_password(data['password']):
+                User.objects.create(
                     email         = data['email'],
                     mobile        = data['mobile'],
                     user_name     = data['user_name'],

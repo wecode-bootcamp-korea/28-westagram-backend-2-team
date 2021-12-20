9eb882824f6ea4fe9ea5e8d516d86166493ab3d3
kang min sung <kangminsung@gangminseong-ui-MacBookPro.local>
2021-12-20 16:30:11 +0900

    띄어쓰기 수정

--- students/minsung/users/views.py
+++ students/minsung/users/views.py
@@ -16,15 +16,19 @@ class UserView(View):
 
         try:
             if email_validator.match(data['email']) == None:
+
                 return JsonResponse({"message": "INVALID EMAIL"}, status=400)
 
             if password_validator.match(data['password']) == None:
+                
                 return JsonResponse({"message": "INVALID PASSWORD"}, status=400)
 
             if User.objects.filter(email=data['email']).exists():
+                
                 return JsonResponse({"message": "DUPLICATED EMAIL"}, status=409)
 
             if User.objects.filter(phone_number=data['phone_number']).exists():
+                
                 return JsonResponse({"message": "DUPLICATED PHONE_NUMBER"}, status=409)
             
             User.objects.create(
@@ -34,7 +38,9 @@ class UserView(View):
                 phone_number  = data['phone_number'],
                 date_of_birth = data['date_of_birth']
             )
+            
             return JsonResponse({"message": "SUCCESS"}, status=201)
         
         except KeyError:
+            
             return JsonResponse({"message": "KEY_ERROR"}, status=400)

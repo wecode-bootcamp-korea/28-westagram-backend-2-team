97ec49d0d105d7910bae4b45d85dc38178cd6be6
chanyoung <cyl0504@gmail.com>
2021-12-21 10:59:33 +0900

    Add : views, validators

--- students/chan/westagram/users/views.py
+++ students/chan/westagram/users/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+import json
+from django.http      import JsonResponse, request
+from django.views     import View
+from users.models     import User
+from users.validators import check_email, check_password
 
-# Create your views here.
+class UsersView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        try:
+            if not check_email(data['email']):
+                return JsonResponse({'Message' : 'INVALID_EMAIL'}, status=400)
+
+            if not check_password(data['password']):
+                return JsonResponse({'Message' : 'INVALID_PASSWORD'}, status=400)
+
+            if User.objects.filter(email=data['email']).exists():
+                return JsonResponse({'Message' : 'EMAIL_EXISTS'}, status=400)
+
+            User.objects.create(
+                name         = data['name'],
+                email        = data['email'],
+                password     = data['password'],
+                phone_number = data['phone_number'],
+                address      = data['address'],
+            )
+
+            return JsonResponse({'Message' : 'SUCCESS'}, status=201)
+
+        except KeyError:
+            return JsonResponse({'Message' : 'KEY_ERROR'}, status=400)

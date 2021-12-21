9b6fe7271939c0024230ce1ba6da7c3705027431
지원석 <jiwonsuk@jiwonseog-ui-MacBook-Air.local>
2021-12-21 12:38:24 +0900

    edited error codes and removed blanks

--- students/wonsuk/westagram/users/views.py
+++ students/wonsuk/westagram/users/views.py
@@ -3,6 +3,7 @@ import json
 from django.core.exceptions import ValidationError
 from django.http            import JsonResponse
 from django.views           import View
+
 from users.models           import User
 from users.validators       import email_regex_match, password_regex_match
 
@@ -15,20 +16,19 @@ class SignUpView(View):
             password_regex_match(data['password'])
             
             if User.objects.filter(email=data['email']).exists():
-                return JsonResponse({'message' : 'EMAIL_ALREADY_EXISTS'}, status=401)
+                return JsonResponse({'message' : 'EMAIL_ALREADY_EXISTS'}, status=400)
 
             User.objects.create(
-                                username     = data['username'],
-                                email        = data['email'],
-                                password     = data['password'],
-                                phone_number = data['phone_number'],
-                                age          = data['age']
-                                )
+                username     = data['username'],
+                email        = data['email'],
+                password     = data['password'],
+                phone_number = data['phone_number'],
+                age          = data['age']
+            )
             return JsonResponse({'message' : 'SUCCESS'}, status=201)
 
         except KeyError:
             return JsonResponse({'message' : 'KEY_ERROR'}, status=400)
         
         except ValidationError as e:
-            return JsonResponse({'message' : e.message}, status=401)
-        
+            return JsonResponse({'message' : e.message}, status=400)
